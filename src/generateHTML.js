da44288ba52327e21a09b87ecb153963e87c2a75
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-08-24 15:16:55 -0400

    updated generateHTML.js email and github username

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -13,10 +13,10 @@ const generateHTML = ({ name, id, email, number, options, github, school }) =>
   <div class="container">
     <h1 class="display-4">Name: ${name}</h1>
     <p class="lead">Employee Id: ${id}</p>
-    <p class="lead">Email: ${email}</p>
+    <p class="lead"><a href="mailto:${email}">Send email</a></p>
     <p class="lead">Office number: ${number}</p>
     <p class="lead">Team member: ${options}</p>
-    <p class="lead">GitHub username: ${github}</p>
+    <p class="lead">GitHub username:<a href = "https://github.com/monicadolce/gen_team" target="_blank"> ${github}</a></p>
     <p class="lead">School name: ${school}</p>
   </div>
 </div>
