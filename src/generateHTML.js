76bdf75916a6e0cd7b506461140a2a12f4570c9c
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-08-25 11:23:26 -0400

    added first card to generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -19,6 +19,19 @@ const generateHTML = ({ name, id, email, number, options, github, school }) =>
     <p class="lead">GitHub username:<a href = "https://github.com/${github}" target="_blank"> ${github}</a></p>
     <p class="lead">School name: ${school}</p>
   </div>
+  <div class="card" style="width: 18rem;">
+  <div class="card-header">
+  <p>${name}</p>
+  <p>${options}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Id: ${id}</li>
+    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+    <li class="list-group-item">Office number: ${number}</li>
+    <li class="list-group-item">GitHub: <a href = "https://github.com/${github}" target="_blank">${github}</a></li>
+    <li class="list-group-item">School: ${school}</li>
+  </ul>
+</div>
 </div>
 </body>
 </html>`;
