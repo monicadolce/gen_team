048ca0386366c0ac88a4ef59bc81f5d715aacb4e
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-08-25 12:07:41 -0400

    trying to link style.css to generateHTML index

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -6,18 +6,13 @@ const generateHTML = ({ name, id, email, number, options, github, school }) =>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./dist/style.css">
   <title>Team Profile</title>
 </head>
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Name: ${name}</h1>
-    <p class="lead">Employee Id: ${id}</p>
-    <p class="lead"><a href="mailto:${email}">Send email</a></p>
-    <p class="lead">Office number: ${number}</p>
-    <p class="lead">Team member: ${options}</p>
-    <p class="lead">GitHub username:<a href = "https://github.com/${github}" target="_blank"> ${github}</a></p>
-    <p class="lead">School name: ${school}</p>
+    <h1 class="display-4 text-center">My Team</h1>
   </div>
   <div class="card" style="width: 18rem;">
   <div class="card-header">
